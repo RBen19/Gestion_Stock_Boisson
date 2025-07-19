c93a9346df31be84a1edfdb0e959381b950cfd5a
RBen19 <bonmerciel@gmail.com>
2025-07-19 20:42:28 +0000

    initial commit

---
+++ src/main/java/org/beni/gestionboisson/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.beni.gestionboisson;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
