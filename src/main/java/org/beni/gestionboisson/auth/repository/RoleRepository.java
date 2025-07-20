3c2748194185916a353080f8f47d4e5f48143945
RBen19 <bonmerciel@gmail.com>
2025-07-20 20:04:25 +0000

    feat(role): define CRUD methods in RoleRepository interface

--- src/main/java/org/beni/gestionboisson/auth/repository/RoleRepository.java
+++ src/main/java/org/beni/gestionboisson/auth/repository/RoleRepository.java
@@ -1,9 +1,18 @@
 package org.beni.gestionboisson.auth.repository;
 
+import org.beni.gestionboisson.auth.dto.RoleDTO;
 import org.beni.gestionboisson.auth.entities.Role;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface RoleRepository {
     Optional<Role> findByCode(String code);
     Role save(Role role);
+    List<Role> findAll();
+    Role getRoleById(Long id);
+    Role createRole(Role role);
+    Role updateRole(Long id, Role role);
+    void deleteRole(Long id);
+    Optional<Role> findById(Long id);
 }
