fbd937ad8653460ceb942cf1fb39fb0e52aed994
RBen19 <bonmerciel@gmail.com>
2025-07-21 04:32:14 +0000

    refactor: update service

--- src/main/java/org/beni/gestionboisson/boisson/service/CategorieService.java
+++ src/main/java/org/beni/gestionboisson/boisson/service/CategorieService.java
@@ -7,5 +7,9 @@ import java.util.List;
 public interface CategorieService {
     List<CategorieDTO> getAllCategories();
     CategorieDTO getCategorieById(Long id);
+    CategorieDTO createCategorie(CategorieDTO categorieDTO);
+    CategorieDTO updateCategorie(Long id, CategorieDTO categorieDTO);
+    void deleteCategorie(Long id);
+    CategorieDTO getCategorieByCode(String codeCategorie);
     void seedCategories();
 }
