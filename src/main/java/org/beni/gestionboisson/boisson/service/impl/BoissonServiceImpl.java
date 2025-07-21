94c1d31bface76d0618186797c76273af936bd94
RBen19 <bonmerciel@gmail.com>
2025-07-21 01:46:07 +0000

    refactor(service): implement HTTP-accessible seeding logic in BoissonServiceImpl

--- src/main/java/org/beni/gestionboisson/boisson/service/impl/BoissonServiceImpl.java
+++ src/main/java/org/beni/gestionboisson/boisson/service/impl/BoissonServiceImpl.java
@@ -56,4 +56,54 @@ public class BoissonServiceImpl implements BoissonService {
                 .map(BoissonMapper::toDTO)
                 .orElse(null);
     }
+
+    @Override
+    @Transactional
+    public void seedBoissons() {
+        // Seed some example boissons
+        List<BoissonDTO> boissonsToSeed = List.of(
+                BoissonDTO.builder().nom("Eau Plate").uniteDeMesure("L").codeCategorie("EAU_PLATE").build(),
+                BoissonDTO.builder().nom("Eau Gazeuse").uniteDeMesure("L").codeCategorie("EAU_GAZEUSE").build(),
+                BoissonDTO.builder().nom("Eau Minérale Cristaline").uniteDeMesure("L").codeCategorie("EAU_MINERALE").build(),
+                BoissonDTO.builder().nom("Eau Citron Menthe").uniteDeMesure("L").codeCategorie("EAU_AROMATISEE").build(),
+
+                BoissonDTO.builder().nom("Coca-Cola").uniteDeMesure("33CL").codeCategorie("SODAS").build(),
+                BoissonDTO.builder().nom("Fanta Orange").uniteDeMesure("33CL").codeCategorie("SODAS").build(),
+                BoissonDTO.builder().nom("Limonade Artisanale").uniteDeMesure("33CL").codeCategorie("LIMONADES").build(),
+                BoissonDTO.builder().nom("Tonic Schweppes").uniteDeMesure("25CL").codeCategorie("TONICS").build(),
+                BoissonDTO.builder().nom("Lipton Ice Tea Pêche").uniteDeMesure("33CL").codeCategorie("THES_GLACES").build(),
+                BoissonDTO.builder().nom("Red Bull").uniteDeMesure("25CL").codeCategorie("BOISSONS_ENERGISANTES").build(),
+
+                BoissonDTO.builder().nom("Jus d'Orange UHT").uniteDeMesure("1L").codeCategorie("JUS_FRUITS_100").build(),
+                BoissonDTO.builder().nom("Nectar de Mangue").uniteDeMesure("1L").codeCategorie("NECTARS_FRUITS").build(),
+
+                BoissonDTO.builder().nom("Lait Entier UHT").uniteDeMesure("1L").codeCategorie("LAIT_NATURE").build(),
+                BoissonDTO.builder().nom("Lait Chocolat UHT").uniteDeMesure("25CL").codeCategorie("LAITS_AROMATISES").build(),
+                BoissonDTO.builder().nom("Lait d'Amande UHT").uniteDeMesure("1L").codeCategorie("BOISSONS_VEGETALES").build(),
+
+                BoissonDTO.builder().nom("Café Expresso en Brique").uniteDeMesure("25CL").codeCategorie("CAFE").build(),
+                BoissonDTO.builder().nom("Thé Noir Infusé").uniteDeMesure("25CL").codeCategorie("THE").build(),
+
+                BoissonDTO.builder().nom("Bière Blonde").uniteDeMesure("33CL").codeCategorie("BIERE_BLONDE").build(),
+                BoissonDTO.builder().nom("Bière Ambrée").uniteDeMesure("33CL").codeCategorie("BIERE_AMBREE").build(),
+
+                BoissonDTO.builder().nom("Vin Rouge Bordeaux").uniteDeMesure("75CL").codeCategorie("VIN_ROUGE").build(),
+                BoissonDTO.builder().nom("Vin Rosé de Provence").uniteDeMesure("75CL").codeCategorie("VIN_ROSE").build(),
+
+                BoissonDTO.builder().nom("Whisky Single Malt").uniteDeMesure("70CL").codeCategorie("WHISKY").build(),
+                BoissonDTO.builder().nom("Vodka Absolut").uniteDeMesure("70CL").codeCategorie("VODKA").build(),
+
+                BoissonDTO.builder().nom("Pastis de Marseille").uniteDeMesure("1L").codeCategorie("PASTIS_ANISE").build(),
+                BoissonDTO.builder().nom("Grand Marnier").uniteDeMesure("70CL").codeCategorie("LIQUEURS").build()
+        );
+
+        for (BoissonDTO boissonDTO : boissonsToSeed) {
+            try {
+                createBoisson(boissonDTO);
+            } catch (Exception e) {
+                // Log the error, but continue seeding other boissons
+                System.err.println("Error seeding boisson: " + boissonDTO.getNom() + " - " + e.getMessage());
+            }
+        }
+    }
 }
