978fa36744ef3542b2dc94739152ba7040827bc3
RBen19 <bonmerciel@gmail.com>
2025-07-22 00:48:35 +0000

    feat(type_emplacement): Add TypeEmplacementNotFoundException

---
+++ src/main/java/org/beni/gestionboisson/type_emplacement/exceptions/TypeEmplacementNotFoundException.java
@@ -0,0 +1,7 @@
+package org.beni.gestionboisson.type_emplacement.exceptions;
+
+public class TypeEmplacementNotFoundException extends RuntimeException {
+    public TypeEmplacementNotFoundException(String message) {
+        super(message);
+    }
+}
