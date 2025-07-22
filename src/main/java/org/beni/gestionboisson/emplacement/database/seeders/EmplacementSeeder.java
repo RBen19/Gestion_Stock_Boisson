c1a30d088ae171a46dc1294e7c4db11410dea39a
RBen19 <bonmerciel@gmail.com>
2025-07-22 00:48:42 +0000

    refactor(emplacement): Replace System.out.println with logger in EmplacementSeeder

--- src/main/java/org/beni/gestionboisson/emplacement/database/seeders/EmplacementSeeder.java
+++ src/main/java/org/beni/gestionboisson/emplacement/database/seeders/EmplacementSeeder.java
@@ -5,12 +5,18 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.beni.gestionboisson.emplacement.dto.EmplacementDTO;
 import org.beni.gestionboisson.emplacement.service.EmplacementService;
+import org.beni.gestionboisson.type_emplacement.controller.TypeEmplacementController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class EmplacementSeeder {
 
     @Inject
     EmplacementService emplacementService;
+    private static final Logger logger = LoggerFactory.getLogger(EmplacementSeeder.class);
+
+
 
     @PostConstruct
     public void init() {
@@ -24,7 +30,7 @@ public class EmplacementSeeder {
             emplacementService.createEmplacement(EmplacementDTO.builder().nom("Zone Réception").codeTypeEmplacement("RECEIVING").build());
             emplacementService.createEmplacement(EmplacementDTO.builder().nom("Stock Principal").codeTypeEmplacement("STOCK").build());
             emplacementService.createEmplacement(EmplacementDTO.builder().nom("Quai Expédition").codeTypeEmplacement("SHIPPING").build());
-            System.out.println("Emplacements seeded.");
+            logger.info("Emplacements seeded.");
         }
     }
 }
