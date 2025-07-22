fdc759240513f41e8e7b39b2f3b8ba2e5e9c3ba9
RBen19 <bonmerciel@gmail.com>
2025-07-22 18:59:13 +0000

    refactor(boisson): update service implementation after unit of measure removal

--- src/main/java/org/beni/gestionboisson/boisson/service/impl/BoissonServiceImpl.java
+++ src/main/java/org/beni/gestionboisson/boisson/service/impl/BoissonServiceImpl.java
@@ -39,8 +39,8 @@ public class BoissonServiceImpl implements BoissonService {
                     return new InvalidBoissonDataException("Invalid Categorie Code");
                 });
 
-        long count = boissonRepository.countByCategoryAndUnit(categorie.getCodeCategorie(), dto.getUniteDeMesure());
-        String codeBoisson = String.format("%s-%s-%03d", categorie.getCodeCategorie().substring(0, 3), dto.getUniteDeMesure(), count + 1);
+        long count = boissonRepository.countByCategory(categorie.getCodeCategorie());
+        String codeBoisson = String.format("%s-%03d", categorie.getCodeCategorie().substring(0, 3), count + 1);
 
         Boisson boisson = BoissonMapper.toEntity(dto);
         boisson.setCodeBoisson(codeBoisson);
@@ -75,39 +75,39 @@ public class BoissonServiceImpl implements BoissonService {
     public void seedBoissons() {
         logger.info("Starting boisson seeding process.");
         List<BoissonDTO> boissonsToSeed = List.of(
-                BoissonDTO.builder().nom("Eau Plate").uniteDeMesure("L").codeCategorie("EAU_PLATE").build(),
-                BoissonDTO.builder().nom("Eau Gazeuse").uniteDeMesure("L").codeCategorie("EAU_GAZEUSE").build(),
-                BoissonDTO.builder().nom("Eau Minérale Cristaline").uniteDeMesure("L").codeCategorie("EAU_MINERALE").build(),
-                BoissonDTO.builder().nom("Eau Citron Menthe").uniteDeMesure("L").codeCategorie("EAU_AROMATISEE").build(),
+                BoissonDTO.builder().nom("Eau Plate").codeCategorie("EAU_PLATE").build(),
+                BoissonDTO.builder().nom("Eau Gazeuse").codeCategorie("EAU_GAZEUSE").build(),
+                BoissonDTO.builder().nom("Eau Minérale Cristaline").codeCategorie("EAU_MINERALE").build(),
+                BoissonDTO.builder().nom("Eau Citron Menthe").codeCategorie("EAU_AROMATISEE").build(),
 
-                BoissonDTO.builder().nom("Coca-Cola").uniteDeMesure("33CL").codeCategorie("SODAS").build(),
-                BoissonDTO.builder().nom("Fanta Orange").uniteDeMesure("33CL").codeCategorie("SODAS").build(),
-                BoissonDTO.builder().nom("Limonade Artisanale").uniteDeMesure("33CL").codeCategorie("LIMONADES").build(),
-                BoissonDTO.builder().nom("Tonic Schweppes").uniteDeMesure("25CL").codeCategorie("TONICS").build(),
-                BoissonDTO.builder().nom("Lipton Ice Tea Pêche").uniteDeMesure("33CL").codeCategorie("THES_GLACES").build(),
-                BoissonDTO.builder().nom("Red Bull").uniteDeMesure("25CL").codeCategorie("BOISSONS_ENERGISANTES").build(),
+                BoissonDTO.builder().nom("Coca-Cola").codeCategorie("SODAS").build(),
+                BoissonDTO.builder().nom("Fanta Orange").codeCategorie("SODAS").build(),
+                BoissonDTO.builder().nom("Limonade Artisanale").codeCategorie("LIMONADES").build(),
+                BoissonDTO.builder().nom("Tonic Schweppes").codeCategorie("TONICS").build(),
+                BoissonDTO.builder().nom("Lipton Ice Tea Pêche").codeCategorie("THES_GLACES").build(),
+                BoissonDTO.builder().nom("Red Bull").codeCategorie("BOISSONS_ENERGISANTES").build(),
 
-                BoissonDTO.builder().nom("Jus d'Orange UHT").uniteDeMesure("1L").codeCategorie("JUS_FRUITS_100").build(),
-                BoissonDTO.builder().nom("Nectar de Mangue").uniteDeMesure("1L").codeCategorie("NECTARS_FRUITS").build(),
+                BoissonDTO.builder().nom("Jus d'Orange UHT").codeCategorie("JUS_FRUITS_100").build(),
+                BoissonDTO.builder().nom("Nectar de Mangue").codeCategorie("NECTARS_FRUITS").build(),
 
-                BoissonDTO.builder().nom("Lait Entier UHT").uniteDeMesure("1L").codeCategorie("LAIT_NATURE").build(),
-                BoissonDTO.builder().nom("Lait Chocolat UHT").uniteDeMesure("25CL").codeCategorie("LAITS_AROMATISES").build(),
-                BoissonDTO.builder().nom("Lait d'Amande UHT").uniteDeMesure("1L").codeCategorie("BOISSONS_VEGETALES").build(),
+                BoissonDTO.builder().nom("Lait Entier UHT").codeCategorie("LAIT_NATURE").build(),
+                BoissonDTO.builder().nom("Lait Chocolat UHT").codeCategorie("LAITS_AROMATISES").build(),
+                BoissonDTO.builder().nom("Lait d'Amande UHT").codeCategorie("BOISSONS_VEGETALES").build(),
 
-                BoissonDTO.builder().nom("Café Expresso en Brique").uniteDeMesure("25CL").codeCategorie("CAFE").build(),
-                BoissonDTO.builder().nom("Thé Noir Infusé").uniteDeMesure("25CL").codeCategorie("THE").build(),
+                BoissonDTO.builder().nom("Café Expresso en Brique").codeCategorie("CAFE").build(),
+                BoissonDTO.builder().nom("Thé Noir Infusé").codeCategorie("THE").build(),
 
-                BoissonDTO.builder().nom("Bière Blonde").uniteDeMesure("33CL").codeCategorie("BIERE_BLONDE").build(),
-                BoissonDTO.builder().nom("Bière Ambrée").uniteDeMesure("33CL").codeCategorie("BIERE_AMBREE").build(),
+                BoissonDTO.builder().nom("Bière Blonde").codeCategorie("BIERE_BLONDE").build(),
+                BoissonDTO.builder().nom("Bière Ambrée").codeCategorie("BIERE_AMBREE").build(),
 
-                BoissonDTO.builder().nom("Vin Rouge Bordeaux").uniteDeMesure("75CL").codeCategorie("VIN_ROUGE").build(),
-                BoissonDTO.builder().nom("Vin Rosé de Provence").uniteDeMesure("75CL").codeCategorie("VIN_ROSE").build(),
+                BoissonDTO.builder().nom("Vin Rouge Bordeaux").codeCategorie("VIN_ROUGE").build(),
+                BoissonDTO.builder().nom("Vin Rosé de Provence").codeCategorie("VIN_ROSE").build(),
 
-                BoissonDTO.builder().nom("Whisky Single Malt").uniteDeMesure("70CL").codeCategorie("WHISKY").build(),
-                BoissonDTO.builder().nom("Vodka Absolut").uniteDeMesure("70CL").codeCategorie("VODKA").build(),
+                BoissonDTO.builder().nom("Whisky Single Malt").codeCategorie("WHISKY").build(),
+                BoissonDTO.builder().nom("Vodka Absolut").codeCategorie("VODKA").build(),
 
-                BoissonDTO.builder().nom("Pastis de Marseille").uniteDeMesure("1L").codeCategorie("PASTIS_ANISE").build(),
-                BoissonDTO.builder().nom("Grand Marnier").uniteDeMesure("70CL").codeCategorie("LIQUEURS").build()
+                BoissonDTO.builder().nom("Pastis de Marseille").codeCategorie("PASTIS_ANISE").build(),
+                BoissonDTO.builder().nom("Grand Marnier").codeCategorie("LIQUEURS").build()
         );
 
         for (BoissonDTO boissonDTO : boissonsToSeed) {
