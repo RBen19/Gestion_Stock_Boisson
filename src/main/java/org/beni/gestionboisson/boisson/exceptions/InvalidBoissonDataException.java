db8f528efe5f6eca38a5e5af8a574a683d1d1796
RBen19 <bonmerciel@gmail.com>
2025-07-22 00:48:34 +0000

    feat(boisson): Add InvalidBoissonDataException

---
+++ src/main/java/org/beni/gestionboisson/boisson/exceptions/InvalidBoissonDataException.java
@@ -0,0 +1,7 @@
+package org.beni.gestionboisson.boisson.exceptions;
+
+public class InvalidBoissonDataException extends RuntimeException {
+    public InvalidBoissonDataException(String message) {
+        super(message);
+    }
+}
