f9e8b11076e9650005ed069d8901e06779889b70
RBen19 <bonmerciel@gmail.com>
2025-07-23 04:21:59 +0000

    feat: create entities

---
+++ src/main/java/org/beni/gestionboisson/type_mouvement/entities/TypeMouvement.java
@@ -0,0 +1,45 @@
+package org.beni.gestionboisson.type_mouvement.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "type_mouvement")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TypeMouvement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String libelle;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = Instant.now();
+        updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
+}
