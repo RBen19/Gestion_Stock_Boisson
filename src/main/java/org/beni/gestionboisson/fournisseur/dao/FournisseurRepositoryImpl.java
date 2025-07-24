90cbe4e6f04d320650f9434dedc26a9014963c16
RBen19 <bonmerciel@gmail.com>
2025-07-24 10:49:19 +0000

    chore(fournisseur): apply consistent repository patterns

--- src/main/java/org/beni/gestionboisson/fournisseur/dao/FournisseurRepositoryImpl.java
+++ src/main/java/org/beni/gestionboisson/fournisseur/dao/FournisseurRepositoryImpl.java
@@ -1,7 +1,9 @@
 package org.beni.gestionboisson.fournisseur.dao;
 
+//import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.PersistenceContext;
