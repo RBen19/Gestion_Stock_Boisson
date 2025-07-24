2c23d02e240522691ab4c44cf95cee581056476a
RBen19 <bonmerciel@gmail.com>
2025-07-24 10:50:28 +0000

    chore(config): tweak application config for dependency setup

--- src/main/java/org/beni/gestionboisson/config/ApplicationConfig.java
+++ src/main/java/org/beni/gestionboisson/config/ApplicationConfig.java
@@ -1,17 +1,30 @@
 package org.beni.gestionboisson.config;
 
 import jakarta.ws.rs.ApplicationPath;
+import org.beni.gestionboisson.HelloResource;
+import org.beni.gestionboisson.auth.security.JwtFilter;
+import org.beni.gestionboisson.auth.security.JwtUtil;
+import org.beni.gestionboisson.auth.service.impl.AuthServiceImpl;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.inject.cdi.se.CdiSeInjectionManagerFactory;
 
-@ApplicationPath("/api")
+/*
+* @ApplicationPath("/api")
 public class ApplicationConfig extends ResourceConfig {
 
     public ApplicationConfig() {
-        //  Active CDI dans Jersey
-        property("jersey.config.inject.InjectManagerFactory", CdiSeInjectionManagerFactory.class.getName());
-
         //  Scanne toutes tes classes REST, filtres, services
         packages("org.beni.gestionboisson");
+        packages("org.beni.gestionboisson.auth");
+        packages("org.beni.gestionboisson.type_lot_status");
+        packages("org.beni.gestionboisson.emplacement");
+        packages("org.beni.gestionboisson.boisson");
+        register(HelloResource.class);
+        register(JwtFilter.class);       // filtre @Provider
+        register(JwtUtil.class);         // utilitaire, doit être injectable
+        register(AuthServiceImpl.class); // service
+        register(JacksonContextResolver.class);
+       // register(org.beni.gestionboisson.type_lot_status.controller.TypeLotStatusController.class);
     }
 }
+* */
+
