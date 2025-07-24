ae99cddb1a205e0896a392be3097d8e285fba974
RBen19 <bonmerciel@gmail.com>
2025-07-24 10:50:27 +0000

    chore(config): adjust HelloApplication for updated security integration

--- src/main/java/org/beni/gestionboisson/HelloApplication.java
+++ src/main/java/org/beni/gestionboisson/HelloApplication.java
@@ -3,7 +3,8 @@ package org.beni.gestionboisson;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+ @ApplicationPath("/api")
 public class HelloApplication extends Application {
 
 }
+
