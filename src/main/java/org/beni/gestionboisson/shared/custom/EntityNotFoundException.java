465cecb7df10f06b462e71291ffc785d344b91f0
RBen19 <bonmerciel@gmail.com>
2025-07-24 10:58:55 +0000

    feat(exception): add EntityNotFoundException for 404 error handling

---
+++ src/main/java/org/beni/gestionboisson/shared/custom/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package org.beni.gestionboisson.shared.custom;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
