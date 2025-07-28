4edda325ae67842080690c7ed9b64bd70738c709
RBen19 <bonmerciel@gmail.com>
2025-07-28 00:36:31 +0000

    feat: add cors origin

---
+++ src/main/java/org/beni/gestionboisson/config/CorsFilter.java
@@ -0,0 +1,24 @@
+package org.beni.gestionboisson.config;
+
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
