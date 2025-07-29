dc32d103b6866318fc0e6e08a01d9d4548bb1c5c
RBen19 <bonmerciel@gmail.com>
2025-07-29 05:34:39 +0000

    feat: update corse

--- src/main/java/org/beni/gestionboisson/config/CorsFilter.java
+++ src/main/java/org/beni/gestionboisson/config/CorsFilter.java
@@ -12,13 +12,26 @@ import java.io.IOException;
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
 
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        // Gérer les requêtes préflight OPTIONS
+        if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+            requestContext.abortWith(
+                    jakarta.ws.rs.core.Response.ok()
+                            .header("Access-Control-Allow-Origin", "http://localhost:5173")
+                            .header("Access-Control-Allow-Credentials", "true")
+                            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-refresh-token")
+                            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                            .build()
+            );
+        }
+    }
 
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
         containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
         containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization,x-refresh-token");
         containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
