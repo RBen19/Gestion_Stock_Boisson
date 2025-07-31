eefe1d4a63571cf31b16b765b790e5813315b802
RBen19 <bonmerciel@gmail.com>
2025-07-31 16:07:24 +0000

    fix: i hope it will fix cors on render

--- src/main/java/org/beni/gestionboisson/config/CorsFilter.java
+++ src/main/java/org/beni/gestionboisson/config/CorsFilter.java
@@ -12,100 +12,37 @@ import java.util.Arrays;
 import java.util.List;
 
 @Provider
-public class CorsFilter   implements ContainerResponseFilter {
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
     private static final List<String> allowedOrigins = Arrays.asList(
             "http://localhost:5173",
             "https://statuesque-crisp-80ec76.netlify.app"
     );
 
     @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        String origin = requestContext.getHeaderString("Origin");
+        
+        if ("OPTIONS".equals(requestContext.getMethod()) && origin != null && allowedOrigins.contains(origin)) {
+            requestContext.abortWith(
+                jakarta.ws.rs.core.Response.ok()
+                    .header("Access-Control-Allow-Origin", origin)
+                    .header("Access-Control-Allow-Credentials", "true")
+                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-refresh-token")
+                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH")
+                    .build()
+            );
+        }
+    }
+
+    @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         String origin = requestContext.getHeaderString("Origin");
 
         if (origin != null && allowedOrigins.contains(origin)) {
-            // Log utile pour debug
-            System.out.println("CORS request from origin: " + origin);
-
-            // On utilise `putSingle` pour éviter les duplications d'en-têtes
             responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", origin);
             responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
             responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-refresh-token");
             responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH");
         }
     }
- /*
- *
- *  public void filter(ContainerRequestContext requestContext) throws IOException {
-        // Gérer les requêtes préflight OPTIONS
-        String origin = requestContext.getHeaderString("Origin");
-        if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")  && allowedOrigins.contains(origin)) {
-            requestContext.abortWith(
-                    jakarta.ws.rs.core.Response.ok()
-                            .header("Access-Control-Allow-Origin", origin)
-                          //  .header("Access-Control-Allow-Origin", "https://statuesque-crisp-80ec76.netlify.app")
-                            .header("Access-Control-Allow-Credentials", "true")
-                            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-refresh-token")
-                            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD,PATCH")
-                            .build()
-            );
-        }
-    }
-
-
-    @Override
-    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
-        String origin = containerRequestContext.getHeaderString("Origin");
-
-        if (origin != null && allowedOrigins.contains(origin)) {
-            containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-            containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization,x-refresh-token");
-            containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD,PATCH");
-            containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
-            //responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-
-        }
-      //  containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:5173");
-     //   containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "https://statuesque-crisp-80ec76.netlify.app");
-
-    }
- *
- * */
-/*
-*
-* private static final List<String> allowedOrigins = Arrays.asList(
-        "http://localhost:5173",
-        "https://statuesque-crisp-80ec76.netlify.app"
-);
-
-    @Override
-    public void filter(ContainerRequestContext requestContext) throws IOException {
-        String origin = requestContext.getHeaderString("Origin");
-
-        if (origin != null && allowedOrigins.contains(origin)
-                && "OPTIONS".equalsIgnoreCase(requestContext.getMethod())) {
-
-            requestContext.abortWith(
-                    jakarta.ws.rs.core.Response.ok()
-                            .header("Access-Control-Allow-Origin", origin)
-                            .header("Access-Control-Allow-Credentials", "true")
-                            .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-refresh-token")
-                            .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH")
-                            .build()
-            );
-        }
-    }
-
-    @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        String origin = requestContext.getHeaderString("Origin");
-
-        if (origin != null && allowedOrigins.contains(origin)) {
-            responseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
-            responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-            responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-refresh-token");
-            responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH");
-        }
-    }
-*
-* */
 }
